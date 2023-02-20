f3ccb760659dee033aa16987cc2245ee202355f6
Hongje Choi <enc2586@gmail.com>
2023-02-20 19:35:43 +0900

    routes and font(html & mui) set

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,21 @@
-import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { ThemeProvider } from '@mui/material/styles'
+// import React from 'react'
+
+import theme from 'themes'
+
+import Home from 'pages/Home'
 
 function App() {
-  return <div></div>
+  return (
+    <ThemeProvider theme={theme}>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home />} />
+        </Routes>
+      </BrowserRouter>
+    </ThemeProvider>
+  )
 }
 
 export default App
---
+++ src/pages/Home.tsx
@@ -0,0 +1,12 @@
+import Button from '@mui/material/Button'
+
+function Home() {
+  return (
+    <div>
+      <h1>Home</h1>
+      <Button>폰트 적용 확인 완료!</Button>
+    </div>
+  )
+}
+
+export default Home
