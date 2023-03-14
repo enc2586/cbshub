8505fcdf2e2e9e327e4949ab68f4f94fb5a3a106
enc2586 <enc2586@gmail.com>
2023-03-15 08:36:56 +0900

    pwa

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import 'assets/global.css'
 
+import * as serviceWorkerRegistration from './serviceWorkerRegistration'
+
+serviceWorkerRegistration.register()
+
 import { InitializeGoogleAnalytics } from 'services/analytics'
 
 if (!window.location.href.includes('localhost')) {
